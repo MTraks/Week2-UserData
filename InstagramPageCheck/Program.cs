c743ae13c9a01ab4c805ab2f12a4b4dd08b84ff1
Martin Traks <martintraks@gmail.com~>
2022-02-14 12:22:19 +0200

    Control Flow

---
+++ InstagramPageCheck/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace InstagramPageCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //progeamm küsib kasutaja vanust;
+            //programm kontrollib, kas kasutaja on vanem kui 13 või 13 aastat vana;
+            //kui kasutaja on 13 aastat vana või vanem, siis ta võib Instagrami kasutada;
+            //kui kasutaja on noorem kui 13, siis ta on liiga noor;
+
+            Console.WriteLine("Palun, sisesta on vanus:");
+            int UserAge = Convert.ToInt32(Console.ReadLine());
+
+            if(UserAge > 13)
+            {
+                Console.WriteLine("Oled piisavalt vana, et Instagrami kontot luua.");
+
+            }
+            else if(UserAge < 13)
+            {
+                Console.WriteLine("Oled liiga noor, et Instagrami kontot luua.");
+            }
+            else
+            {
+                Console.WriteLine("Oled 13 aastat vana, nüüd saad Instagrami kontot luua.");
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+
+
+
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaasta;
+            //programm kuvab sünniaasta konsoolis;
+            //"oled sündinud aastal {yearOfBirth}";
+
+            Console.WriteLine("Palun, sisesta oma vanus:");
+            int UserAge = Convert.ToInt32(Console.ReadLine());
+
+            int YearOfBireth = 2021 - UserAge;
+
+            Console.WriteLine($"Sa oled sündinud aastal {YearOfBireth}.");
+
+
+
+
+        }
+    }
+}
